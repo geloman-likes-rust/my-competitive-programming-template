3c60c676a128627ff4c2a5a46a9a67376da30f58
Mark Angelo Torres <tmarkangelo96@gmail.com>
2022-10-13 02:53:45 +0800

    cp-template

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+void solve() {
+}
+
+int main ()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
